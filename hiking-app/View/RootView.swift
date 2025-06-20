c809bf390ae63a3e578571e051a7be7579c87d74
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-20 23:48:51 +0300

    graqhQL init

---
+++ hiking-app/View/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  hiking-app
+//
+//  Created by Oleksandr Rolik on 18.06.2025.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}

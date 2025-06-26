af24d05c4f6da9b9d7061548f0be2833663786ca
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-26 19:47:56 +0300

    init

--- hiking-app/View/RootView.swift
+++ hiking-app/View/RootView.swift
@@ -1,15 +1,9 @@
-//
-//  RootView.swift
-//  hiking-app
-//
-//  Created by Oleksandr Rolik on 18.06.2025.
-//
-
 import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hike!")
+            .bold()
     }
 }
 

27ad13b907ab1346bd1bd28432dfdabbe91b426b
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-07-19 14:15:39 +0300

    Initial Commit

---
+++ hiking-app/hiking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiking_appApp.swift
+//  hiking-app
+//
+//  Created by Oleksandr Rolik on 19.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct hiking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

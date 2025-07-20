fa1fc669550a01877c40ffb7123f42cf300987d1
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-07-20 16:07:00 +0300

    mapbox init

--- hiking-app/hiking_appApp.swift
+++ hiking-app/hiking_appApp.swift
@@ -1,17 +1,10 @@
-//
-//  hiking_appApp.swift
-//  hiking-app
-//
-//  Created by Oleksandr Rolik on 19.07.2025.
-//
-
 import SwiftUI
 
 @main
 struct hiking_appApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MapView()
         }
     }
 }

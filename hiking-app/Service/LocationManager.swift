9a363ee905659ea815df90b53ef4e13970a1d925
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-08-05 00:30:53 +0300

    edit location manager,  geocode city name by coordinates

--- hiking-app/Service/LocationManager.swift
+++ hiking-app/Service/LocationManager.swift
@@ -5,9 +5,11 @@ import CoreLocation
 final class LocationManager: NSObject, ObservableObject {
     @Published var location: Location = Location(coordinate: CLLocationCoordinate2D(latitude: 50.27, longitude: 30.31))
     @Published var heading: Heading = Heading(direction: 0, accuracy: 0)
-
+    @Published var cityName: String = "Unknown"
+    
     private let manager = CLLocationManager()
-
+    private let geocoder = CLGeocoder()
+    
     override init() {
         super.init()
         manager.delegate = self
@@ -21,15 +23,28 @@ final class LocationManager: NSObject, ObservableObject {
 extension LocationManager: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let clLocation = locations.last else { return }
-
+        
         DispatchQueue.main.async {
             self.location = Location(coordinate: clLocation.coordinate)
+            self.getCityName(location: clLocation)
         }
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         DispatchQueue.main.async {
             self.heading = Heading(direction: newHeading.trueHeading, accuracy: newHeading.headingAccuracy)
+        }
+    }
+    
+    private func getCityName(location: CLLocation) {
+        geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, error in
+            guard let placemark = placemarks?.first, error == nil else { return }
+            
+            if let city = placemark.locality {
+                DispatchQueue.main.async {
+                    self?.cityName = city
+                }
+            }
         }
     }
 }

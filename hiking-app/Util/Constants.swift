2daad920508b7fa87197920d3ce97afc6ca7fb53
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-08-04 23:19:39 +0300

    weather service for get data from API

--- hiking-app/Util/Constants.swift
+++ hiking-app/Util/Constants.swift
@@ -4,8 +4,7 @@ enum Constants {
     
     enum WeatherApi {
         static let weatherAPIKey = EnvParser.shared.value(fileName: "keys.env", value: "OPEN_WEATHER_FORECAST_API_KEY") ?? ""
-        static let fiveDaysForecastAPIQuery = EnvParser.shared.value(fileName: "keys.env", value: "API_QUERY_5DAYS_FORECAST") ?? ""
-        static let currentWeatherAPIQuery = EnvParser.shared.value(fileName: "keys.env", value: "API_QUERY_CURRENT_DAY_FORECAST") ?? ""
+        static let forecastAPIQuery = EnvParser.shared.value(fileName: "keys.env", value: "API_QUERY_FORECAST") ?? ""
         
         static let fiveDaysForecastEndPoint = "forecast"
         static let currentDayForecastEndPoint = "weather"

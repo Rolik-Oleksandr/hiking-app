bbce3d52f13889732e7997eada122f294e0229bc
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-08-04 23:04:29 +0300

    extend Constants file

--- hiking-app/Util/Constants.swift
+++ hiking-app/Util/Constants.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 enum Constants {
-    static let weatherAPIKey = EnvParser.shared.value(fileName: "keys.env", value: "OPEN_WEATHER_FORECAST_API_KEY") ?? ""
+    
+    enum WeatherApi {
+        static let weatherAPIKey = EnvParser.shared.value(fileName: "keys.env", value: "OPEN_WEATHER_FORECAST_API_KEY") ?? ""
+        static let fiveDaysForecastAPIQuery = EnvParser.shared.value(fileName: "keys.env", value: "API_QUERY_5DAYS_FORECAST") ?? ""
+        static let currentWeatherAPIQuery = EnvParser.shared.value(fileName: "keys.env", value: "API_QUERY_CURRENT_DAY_FORECAST") ?? ""
+        
+        static let fiveDaysForecastEndPoint = "forecast"
+        static let currentDayForecastEndPoint = "weather"
+    }
+    
+    
 }

9b4b507a1a7707d9768569f9c509bbe572e59266
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-08-08 23:17:24 +0300

    init location and weather bar, using KingFisher for cache image from API

--- hiking-app/View/MainScreen/CurrentLocationAndWeatherForecastBarView.swift
+++ hiking-app/View/MainScreen/CurrentLocationAndWeatherForecastBarView.swift
@@ -1,8 +1,11 @@
 import SwiftUI
+import Kingfisher
 
 struct CurrentLocationAndWeatherForecastBarView: View {
     @ObservedObject var locationManager = LocationManager()
     @ObservedObject var weatherViewModel: WeatherForecastViewModel
+    
+    @State private var showFiveDayForecast = false
     
     var body: some View {
         HStack{
@@ -14,31 +17,31 @@ struct CurrentLocationAndWeatherForecastBarView: View {
             Spacer()
             
             if let weather = weatherViewModel.currentWeather,
-               let iconURL = weather.iconURL {
+               let iconURL = weather.currentForecastIconURL {
                 
                 HStack {
                     Text("\(weather.main.roundedTemp)°")
                     
-                    AsyncImage(url: iconURL) { phase in
-                        switch phase {
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                        case .failure:
-                            Image(systemName: "questionmark.circle")
-                        case .empty:
-                            ProgressView()
-                        @unknown default:
-                            EmptyView()
-                        }
-                    }
+                    KFImage(iconURL)
+                        .cacheOriginalImage()
+                        .resizable()
+                        .frame(width: 50, height: 50)
                 }
             }
         }
         .onAppear {
             weatherViewModel.getCurrentForecast(city: locationManager.cityName)
             print(locationManager.cityName)
+        }
+        .onTapGesture {
+            showFiveDayForecast = true
+        }
+        .sheet(isPresented: $showFiveDayForecast) {
+            ForecastDetailView(
+                showFiveDayForecast: $showFiveDayForecast,
+                forecast: weatherViewModel.forecast,
+                currentLocation: locationManager.cityName
+            )
         }
         .padding()
         .frame(width: 350, height: 70)
@@ -47,4 +50,40 @@ struct CurrentLocationAndWeatherForecastBarView: View {
 
 #Preview {
     CurrentLocationAndWeatherForecastBarView(weatherViewModel: WeatherForecastViewModel())
+}
+
+struct ForecastDetailView: View {
+    @Binding var showFiveDayForecast: Bool
+    let forecast: [ForecastEntry]
+    let currentLocation: String
+    
+    var body: some View {
+        NavigationView {
+            ScrollView(.horizontal) {
+                HStack() {
+                    ForEach(forecast.prefix(40), id: \.dt) { entry in
+                        VStack(alignment: .leading) {
+                            Text("\(entry.localizedDayOfWeek) \(entry.timeString)")
+                                .font(.headline)
+                            Text("🌡 \(entry.main.temp, specifier: "%.0f") °C")
+                                .font(.subheadline)
+                        }
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .shadow(radius: 2)
+                        .frame(width: 140)
+                    }
+                }
+            }
+            .navigationTitle("Прогноз на \(currentLocation)")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Закрити") {
+                        showFiveDayForecast = false
+                    }
+                }
+            }
+        }
+    }
 }
